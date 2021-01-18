7ddbd3fc13677845706373b6807737106884df79
Nick Pesce <nickpesce22@gmail.com>
2021-01-18 15:09:27 -0500

    Send status

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const app = express();
 const iot = require('@google-cloud/iot');
+const { OAuth2Client } = require('google-auth-library');
+CLIENT_ID = '168837114669-ucclhidtfd6bh08tv0fsefajvoflh75u.apps.googleusercontent.com';
+const oauthClient = new OAuth2Client(CLIENT_ID);
 
 const iotClient = new iot.v1.DeviceManagerClient({});
+
+app.use(express.json());
 
 projectId = 'lit-illumination-technology';
 cloudRegion = 'us-central1';
 registryId = 'lights';
 deviceId = 'test-device';
 
+async function verify(token) {
+    const ticket = await oauthClient.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID,
+    });
+    const payload = ticket.getPayload();
+    return payload;
+}
 
 app.get('/', (req, res) => {
-    const name = process.env.NAME || 'World';
-    res.send(`Hello ${name}!`);
+    res.sendFile('index.html', { root: __dirname });
+});
+
+app.post('/api/v1/on_login', (req, res) => {
+    verify(req.body.id_token).then((data) => { console.log(JSON.stringify(data)); res.sendStatus(200); }).catch(reason => { console.log(reason); res.sendStatus(403); });
 });
 
 app.post('/api/v1/effects', (req, res) => {
@@ -33,7 +49,7 @@ app.post('/api/v1/effects', (req, res) => {
         iotClient.sendCommandToDevice(request).then((response, _) => { console.log(response); res.send(JSON.stringify(response)) }).catch((reason) => { console.log(reason); res.status(500) });
     } catch (err) {
         console.error('Could not send command:', err);
-        res.status(500);
+        res.sendStatus(500);
     }
 });
 
